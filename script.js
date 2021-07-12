e83e565faf52555d26f31a8238bb0a7e1e347dd9
npaillasson <nathanpaillasson3@gmail.com>
2021-07-12 14:44:13 +0200

    rewrite of the api data retrieval function

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ var bestMovies = {
 	bestMoviesArray: []
 };
 
-var bestActionMovies = {
+var firstCategorie = {
 	genre: "Action",
 	moviesIds: [],
 	bestActionMoviesArray: []
 };
 
-var bestMysteryMovies = {
+var secondeCategorie = {
 	genre: "Mystery",
 	moviesIds: [],
 	bestMysteryMoviesArray: []
 };
 
-var bestFantasyMovies = {
+var thirdCategorie = {
 	genre: "Fantasy",
 	moviesIds: [],
 	bestFantasyMoviesArray: []
@@ -30,59 +30,15 @@ let nextPage = "&page=2"
 
 
 function getUrl(moviesObject) {
-	let url_page_1 = endPoint + startFilterUrl + moviesObject.genre + endFilterUrl;
-	let url_page_2 = endPoint + startFilterUrl + moviesObject.genre + endFilterUrl + nextPage;
-	return [url_page_1, url_page_2];
+let genre = moviesObject.genre
+let urlFirstPage = endPoint + startFilterUrl + moviesObject.genre + endFilterUrl
+let urlSecondePage = urlFirstPage + nextPage;
+let returnArray = [urlFirstPage, urlSecondePage];
+return returnArray
 }
 
-function getRequest(url) {
-	newArray = [];
-	fetch(url)
-		.then(res => {
-			if (res.ok) {
-				return res.json().then(data => {
-					newArray = data["results"]
-					console.log("premierArray:", newArray )
-				})
-			} else {
-			console.log("erreur");
-			}
-		})
-}
-
-
-function getIDlist(moviesObject) {
-	let urlArray = getUrl(moviesObject);
-	let idList = [];
-	let newObject = [];
-	for (url of urlArray){
-	fetch(url)
-	  .then(function(res) {
-	    if (res.ok) {
-	      return res.json();
-	    }
-	  })
-	  .then(function(value) {
-	    newObject = (value["results"]);
-	    for (id of newObject) {
-	    	idList.push(id)
-	    }
-	  return idList
-	  })
-	  .catch(function(err) {
-	    // Une erreur est survenue
-	  });
-	  console.log(idList)
-
-	}
-	console.log(idList)
-	return idList
-}
-
-bestMovies["moviesIds"] = getIDlist(bestMovies);
-let test = bestMovies.moviesIds[0]
-console.log("test", test)
-console.log(bestMovies["moviesIds"])
+let test = getUrl(bestMovies);
+console.log(test[1])
 
 
 class Film {
