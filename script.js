533543d9ad2eec0eea7bcddb9e0c3d17697c4a14
npaillasson <nathanpaillasson3@gmail.com>
2021-07-15 21:54:54 +0200

    modal window can ben opened and closed

--- script.js
+++ script.js
@@ -104,8 +104,33 @@ class Film {
 	}
 }
 
-document.querySelectorAll(".bloc_illustration").forEach(section => {
-	console.log(section)
-	section.addEventListener("click", openmodal())
+const closemodal = function (e) {
+	e.preventDefault()
+	modalWindow = document.getElementById("modal1")
+	modalWindow.style.display = "none";
+	modalWindow.setAttribute("aria-hidden", true)
+	modalWindow.setAttribute("aria-modal", false)
+}
+
+
+const openmodal = function (e) {
+	e.preventDefault()
+	closeButton = document.getElementById("close_button")
+	modalWindow = document.getElementById("modal1")
+	modalWindow.style.display = "flex";
+	modalWindow.setAttribute("aria-hidden", false)
+	modalWindow.setAttribute("aria-modal", true)
+	closeButton.addEventListener("click", closemodal)
+
+
+
+}
+
+document.querySelectorAll(".film").forEach(img => {
+	img.addEventListener("click", openmodal)
 })
+
+let a = document.querySelectorAll(".film");
+console.log(a)
+
 
