61a3248368dfdcf55d0edea14798c2f69a0557c1
npaillasson <nathanpaillasson3@gmail.com>
2021-07-17 01:17:03 +0200

    first functionnal version of the website

--- modal_management.js
+++ modal_management.js
@@ -1,6 +1,12 @@
 
 
-//console.log(test)
+function makeBulletsList (infoArray) {
+	let returnValue = "";
+	for (let bullet of infoArray) {
+		returnValue = returnValue + `<li>${bullet}</li>`
+	}
+	return returnValue
+}
 
 const closemodal = function (e) {
 	e.preventDefault()
@@ -21,6 +27,19 @@ const openmodal = function (e) {
 	const film_id = e.target.getAttribute("id");
 	let url = `http://localhost:8000/api/v1/titles/${film_id}`
 	fetch(url).then(response => response.json().then(data => {
+		modalWindow["title"]
+		document.getElementById("title").innerText = data["title"]
+		document.getElementById("genres").innerHTML = makeBulletsList(data["genres"])
+		document.getElementById("date").innerText = data["date_published"]
+		document.getElementById("rated").innerText = data["rated"]
+		document.getElementById("image_modal").src = data["image_url"]
+		document.getElementById("imdb_score").innerText = data["imdb_score"]
+		document.getElementById("directors").innerHTML = makeBulletsList(data["directors"])
+		document.getElementById("countries").innerHTML = makeBulletsList(data["countries"])
+		document.getElementById("actors").innerHTML = makeBulletsList(data["actors"])
+		document.getElementById("duration").innerText = data["duration"] + " minutes"
+		document.getElementById("worldwide_gross_income").innerText = data["worldwide_gross_income"] + " $"
+		document.getElementById("description").innerText = data["description"]
 
 	}))
 	console.log(url)
@@ -36,3 +55,16 @@ document.querySelectorAll(".film").forEach(img => {
 
 let a = document.querySelectorAll(".film");
 console.log(a)
+//
+//	this.image_url = image_url
+//	this.title
+//	this.genres
+//	this.date_published
+//	this.rated
+//	this.imdb_score
+//	this.directors
+//	this.actors
+//	this.duration
+//	this.countries
+//	this.worldwide_gross_income
+//	this.description
